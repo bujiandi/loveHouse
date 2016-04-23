7dda3866b54d00a246b0ce1928b3936865edf833
慧趣小歪 <huiquhome@163.com>
2016-04-23 17:03:40 +0800

    首页

---
+++ loveHouse/PopupController.swift
@@ -0,0 +1,36 @@
+//
+//  PopupController.swift
+//  loveHouse
+//
+//  Created by 慧趣小歪 on 16/4/23.
+//  Copyright © 2016年 C Lau. All rights reserved.
+//
+
+import UIKit
+
+class PopupController: UIViewController {
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- loveHouse/ViewController.swift
+++ loveHouse/ViewController.swift
@@ -10,12 +10,40 @@ import UIKit
 
 class ViewController: UIViewController,UIPopoverPresentationControllerDelegate {
     
+    @IBOutlet weak var searchButton:UIButton!
+    @IBOutlet weak var topContainer:UIView!
+    @IBOutlet weak var shadowView:UIView!
+    @IBOutlet weak var tabBar:UITabBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // 设置搜索按钮圆角
+        searchButton.layer.cornerRadius  = 4
+     
+        // 设置按钮容器圆角 且 子视图不超过圆角范围
+        topContainer.layer.masksToBounds = true
+        topContainer.layer.cornerRadius = 3
         
+        // 设置阴影视图背景透明且 增加引用
+        shadowView.backgroundColor = UIColor.clearColor()
+        shadowView.layer.shadowOffset  = CGSizeMake(0, 1)
+        shadowView.layer.shadowColor   = UIColor.grayColor().CGColor
+        shadowView.layer.shadowRadius  = 4
+        shadowView.layer.shadowOpacity = 0.8
+        
+        // 给底部工具条添加阴影
+        tabBar.layer.shadowOffset  = CGSizeMake(0, 1)
+        tabBar.layer.shadowColor   = UIColor.grayColor().CGColor
+        tabBar.layer.shadowRadius  = 4
+        tabBar.layer.shadowOpacity = 0.8
+        
+        // 默认选中第一项
+        tabBar.selectedItem = tabBar.items?.first
     }
     
+    @IBAction func onPopupButtonTapped(button:UIButton) {
+        performSegueWithIdentifier("popoverSegue", sender: button)
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -27,7 +55,10 @@ class ViewController: UIViewController,UIPopoverPresentationControllerDelegate {
             let popoverViewController = segue.destinationViewController 
             popoverViewController.modalPresentationStyle = UIModalPresentationStyle.Popover
             popoverViewController.popoverPresentationController!.delegate = self
-            popoverViewController.popoverPresentationController?.sourceRect = CGRectMake(sender!.frame.origin.x, 0, sender!.frame.width, sender!.frame.height)
+            popoverViewController.view.frame.size.width = view.bounds.width
+            var frame = sender!.frame
+            frame.origin.y = 0
+            popoverViewController.popoverPresentationController?.sourceRect = frame
         }
     }
     
